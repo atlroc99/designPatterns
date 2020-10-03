32e8aabcb434a530c5deb7cac563b8c8ed8bf27a
Mohammad Zaman <mohammadzaman@Mohammads-MacBook-Pro.local>
2020-10-03 11:23:15 -0400

    added two behavioral patterns and classes

---
+++ src/com/design/pattern/behavioral_3/observer/ObserverMain.java
@@ -0,0 +1,6 @@
+package com.design.pattern.behavioral_3.observer;
+
+public class ObserverMain {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/design/pattern/behavioral_3/strategy/StrategyMain.java
@@ -0,0 +1,6 @@
+package com.design.pattern.behavioral_3.strategy;
+
+public class StrategyMain {
+    public static void main(String[] args) {
+    }
+}
